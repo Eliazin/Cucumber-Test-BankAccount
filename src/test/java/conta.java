114d2325cffaa9646a375e7a2ba441353d6de7b2
Eliazin <eliezerpinheiro.sor@gmail.com>
2022-11-04 13:27:47 -0300

    Etapa 3: Programação e documentação da classe conta.java

--- src/test/java/conta.java
+++ src/test/java/conta.java
@@ -3,39 +3,106 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class conta {
+
+	int clienteEspecial;
+	int clienteComum;
+	int saque;
+	int saldoAtual = saque - clienteEspecial;
+	boolean tipoCliente;
+	
+	
+	/**
+	 * 
+	 * @param int1 Método para um cliente especial com saldo negativado
+	 */
 	@Given("Um cliente especial com saldo atual de {int} reais")
 	public void um_cliente_especial_com_saldo_atual_de_reais(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+		// Write code here that turns the phrase above into concrete actions
+		this.clienteEspecial = int1;
 
+		if (this.clienteEspecial != int1) {
+
+			throw new io.cucumber.java.PendingException();
+		}
+	}
+	
+	
+	/**
+	 * 
+	 * @param int2 Método para quando o cliente especial negativado solicitar um saque
+	 */
 	@When("For solicitado um saque no valor de {int} reais")
-	public void for_solicitado_um_saque_no_valor_de_reais(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void for_solicitado_um_saque_no_valor_de_reais(Integer int2) {
+		// Write code here that turns the phrase above into concrete actions
+		this.saque = int2;
+
+		if (this.saque != int2) {
+			throw new io.cucumber.java.PendingException();
+		}
 	}
 
+	/**
+	 * 
+	 * @param int3 Método para quando o cliente especial negativado solicitar um saque, o banco aceitará porém aumentará a dívida
+	 */
 	@Then("deve efetuar o saque e atualizar o saldo da conta para {int} reais")
-	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer int3) {
+		// Write code here that turns the phrase above into concrete actions
+
+		this.tipoCliente = true;
+		if (tipoCliente = true && this.clienteEspecial < 0) {
+			this.saldoAtual = int3;
+		} else {
+			throw new io.cucumber.java.PendingException();
+		}
 	}
 
+	
+	/**
+	 * 
+	 * @param int1 Método para um cliente comum com saldo negativado
+	 */
 	@Given("Um cliente comum com saldo atual de {int} reais")
 	public void um_cliente_comum_com_saldo_atual_de_reais(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		// Write code here that turns the phrase above into concrete actions
+
+		this.clienteComum = int1;
+
+		if (this.clienteComum != int1) {
+
+			throw new io.cucumber.java.PendingException();
+		}
 	}
 
+	/**
+	 * 
+	 * @param int2 Método para quando um cliente comum com saldo negativado solicitar o saque
+	 */
 	@When("Solicitar um saque de {int} reais")
-	public void solicitar_um_saque_de_reais(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void solicitar_um_saque_de_reais(Integer int2) {
+		// Write code here that turns the phrase above into concrete actions
+		
+		this.saque = int2;
+		
+		if(this.saque != int2) {
+		
+		throw new io.cucumber.java.PendingException();
+		}
 	}
 
+	
+	/**
+	 * 
+	 * @param Método para quando um cliente comum com saldo negativado solicitar o saque, o banco negará o saque e mostrará a mensagem "Saldo insuficiente"
+	 */
 	@Then("Nao deve efetuar o saque e deve retornar a mensagem saldo insuficiente")
 	public void nao_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_saldo_insuficiente() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		
+		if(this.tipoCliente = true) {
+			System.out.print("Saldo insuficiente");
+		}else {
+		throw new io.cucumber.java.PendingException();
+		}
 	}
 }
